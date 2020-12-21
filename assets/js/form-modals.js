3949eb5c2f50e49cd4b91cbabf9508dce48e8adb
Donald Ferracci <donald.tsp@gmail.com>
2020-12-21 11:58:04 -0500

    Added TSP-U-1 modal.

--- assets/js/form-modals.js
+++ assets/js/form-modals.js
@@ -3,12 +3,13 @@
 
 function addFormModals() {
   $('a[href$="tsp-1.pdf"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tsp01');
+  $('a[href$="tsp-u-1.pdf"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tspu01');
   $('a[href$="tsp-3.pdf"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tsp03');
   $('a[href$="tsp09"]').attr('data-toggle', 'modal').attr('data-target', '#tsp09');
   $('a[href$="tsp-15.pdf"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tsp15');
   $('a[href$="tsp20"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tsp20');
   $('a[href$="tsp-25.pdf"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tsp25');
-  // $('a[href$="tsp-92.pdf"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tsp92');  
+  // $('a[href$="tsp-92.pdf"]').not('.form').attr('data-toggle', 'modal').attr('data-target', '#tsp92');
 }
 
 /* CLICK FUNCTIONS FOR TSP-9, TSP-15, and TSP-25 Yes-No BUTTONS */
