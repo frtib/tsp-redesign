38e5d83e6606d38621cb278ab9335867d3f05576
Donald Ferracci <donald.tsp@gmail.com>
2021-02-09 16:46:34 -0500

    Added redirect to Monthly Annuity Factors for R. Raymo on 2/9/2021.

---
+++ assets/js/redirect-to.js
@@ -0,0 +1,6 @@
+// /MonthlyEstimate/
+function monthlyEstimate() {
+  window.setTimeout(function() {
+    window.location.href = 'https://secure.tsp.gov/components/CORS/monthlyAnnuityFactors.html';
+  }, 5000);
+};
